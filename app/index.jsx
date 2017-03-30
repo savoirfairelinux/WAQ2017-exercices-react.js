1e153e06a153780dfeddfa937e42d95d581ec6f9
Alexis Piéplu <alexis.pieplu@savoirfairelinux.com>
2017-03-30 18:57:55 -0400

    Add presa 5 example

--- app/index.jsx
+++ app/index.jsx
@@ -7,8 +7,9 @@ import ex3 from './components/ex-3-propstype.jsx';
 import ex4 from './components/ex-4-propschildren.jsx';
 import ex5 from './components/ex-5-counter.jsx';
 import ex6 from './components/ex-6-lifecycle.jsx';
-import ex8 from './components/ex-8-ref.jsx';
-import ex9 from './components/ex-9-stateless.jsx';
+import ex8 from './components/ex-8-stateless.jsx';
+import ex9 from './components/ex-9-statefull.jsx';
+import ex10 from './components/ex-10-ref.jsx';
 import {
   BrowserRouter as Router,
   Route,
@@ -35,8 +36,9 @@ ReactDOM.render((
               <li><Link to="/4">4 PropsChildren</Link></li>
               <li><Link to="/5">5 Counter</Link></li>
               <li><Link to="/6">6 Lifecycle</Link></li>
-              <li><Link to="/8">8 Ref</Link></li>
-              <li><Link to="/9">9 Stateless</Link></li>
+              <li><Link to="/8">8 Stateless</Link></li>
+              <li><Link to="/9">9 Statefull</Link></li>
+              <li><Link to="/10">10 Ref</Link></li>
             </ul>
           </div>
         </div>
@@ -52,6 +54,7 @@ ReactDOM.render((
         <Route path="/6" component={ex6}/>
         <Route path="/8" component={ex8}/>
         <Route path="/9" component={ex9}/>
+        <Route path="/10" component={ex10}/>
       </div>
     </div>
   </Router>
